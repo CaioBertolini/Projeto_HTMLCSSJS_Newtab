1d2bda7d2bb1524e71484f30ab73b4218cd13c8b
Caio Bertolini <caio.bertolini58@gmail.com>
2021-04-07 21:15:54 -0300

    Adicionando Mask

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,4 @@
+// Modificação da tela em relação o hamburguer menu
 function myFunction(x) {
     if (x.matches) {
         document.getElementById("menuHeader").style.display = "flex";
@@ -6,57 +7,82 @@ function myFunction(x) {
     }
 }
 
+var x = window.matchMedia("(min-width: 768px)")
+myFunction(x)
+x.addListener(myFunction)
 
-function hambMenu(){
+
+// Abrir e fechar o hamburguer menu
+function hambMenu() {
     document.getElementById("menuHeader").style.display = "flex";
 }
 
-function closeMenu(){
+function closeMenu() {
     document.getElementById("menuHeader").style.display = "none";
 }
 
-
-function validaForm(){
+// Checar se o form está preenchido
+function validaForm() {
     if (document.getElementById("nameCommodity").value == '') {
         document.getElementById("mercNameNotice").style.display = "block";
         return false
     } else {
         document.getElementById("mercNameNotice").style.display = "none";
-    };
-    
-    if (document.getElementById("valueCommodity").value == ''){
-        document.getElementById("mercValueNotice").style.display = "block";
-    } else {
-        document.getElementById("mercValueNotice").style.display = "none";
-    };
+        if (document.getElementById("valueCommodity").value == '') {
+            document.getElementById("mercValueNotice").style.display = "block";
+            return false
+        } else {
+            document.getElementById("mercValueNotice").style.display = "none";
+        }
+    }
+    document.querySelector("form").reset()
 }
 
+// Mascará para número
 const input = document.getElementById("valueCommodity");
 let num = "";
 
 function checkNum(e) {
     e.preventDefault();
-    if(["0","1","2","3","4","5","6","7","8","9","Backspace"].indexOf(e.key) == -1){
-        console.log("Letra: "+ e.key);
+    if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Backspace"].indexOf(e.key) == -1) {
+        console.log("Letra: " + e.key);
     } else {
         num += e.key;
-        if(e.key == "Backspace"){
-            num = num.slice(0,-10)
-        } else {}
-        if(num.length == 0){
+        if (e.key == "Backspace") {
+            num = num.slice(0, -10)
+        }
+        if (num.length == 0) {
             e.target.value = ""
-        } else if (num.length < 2){
+        } else if (num.length < 2) {
             e.target.value = "0,0" + num;
-        } else if (num.length < 3){
+        } else if (num.length < 3) {
             e.target.value = "0," + num;
         } else {
-            e.target.value = num.slice(0,-2) + ',' + num.slice(-2);
+            let divi = (num.length - 2) / 3
+            let pontoDivi = divi.toString().split('.');
+            if ( divi <= 1) {
+                e.target.value = num.slice(0, -2) + ',' + num.slice(-2);
+            } else {
+                let numPoint = ""
+                if(Number.isInteger(divi)){
+                    for(let i=0;i<(parseInt(pontoDivi[0],10)-1);i++) {
+                        let numRest = ((num.length - 2) % 3) + (i*3)
+                        numPoint += "." + num.slice((numRest),(numRest+3))
+                    }
+                    e.target.value = num.slice(0, 3) + numPoint + ',' + num.slice(-2);
+                } else {
+                    for(let i=0;i<parseInt(pontoDivi[0],10);i++) {
+                        let numRest = ((num.length - 2) % 3) + (i*3)
+                        numPoint += "." + num.slice((numRest),(numRest+3))
+                    }
+                    let restNum = (num.length - 2) % 3
+                    e.target.value = num.slice(0, restNum) + numPoint  + ',' + num.slice(-2);
+                }
+                
+            }
+            
         }
     }
 }
 
 input.addEventListener('keydown', checkNum);
-
-var x = window.matchMedia("(min-width: 768px)")
-myFunction(x)
-x.addListener(myFunction)
