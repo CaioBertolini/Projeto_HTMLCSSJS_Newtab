ca28b0f27984392f2bec843823dede56c70d7a00
CaioBertolini <caio.bertolini58@gmail.com>
2021-04-08 22:24:47 -0300

    Modificação da máscara

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,18 +1,18 @@
 // Declaração das variáveis do form
-let numSum = 0.0
+let numSum = 0.0;
 
 // Verificando se a div com os resultados está vazia
-let div = document.getElementById("dealResul").innerHTML.trim().length
-let divEmpty = 0
+let div = document.getElementById("dealResul").innerHTML.trim().length;
+let divEmpty = 0;
 if (div == 0){
     document.getElementById("dealResul").innerHTML +=`
         <div class="merc" style="width: 100%; text-align: center;">
             <span style="width: 100%;"> Insira um valor de transação</span>
         </div>
-        `
-    divEmpty = 1
-    document.getElementById("totalValue").innerHTML ="R$ 0,00"
-}
+        `;
+    divEmpty = 1;
+    document.getElementById("totalValue").innerHTML ="R$ 0,00";
+};
 
 
 // Modificação da tela em relação o hamburguer menu
@@ -20,47 +20,47 @@ function myFunction(x) {
     if (x.matches) {
         document.getElementById("menuHeader").style.display = "flex";
     } else {
-        document.getElementById("menuHeader").style.display = "none"
-    }
-}
+        document.getElementById("menuHeader").style.display = "none";
+    };
+};
 
-var x = window.matchMedia("(min-width: 768px)")
-myFunction(x)
-x.addListener(myFunction)
+var x = window.matchMedia("(min-width: 768px)");
+myFunction(x);
+x.addListener(myFunction);
 
 // Abrir e fechar o hamburguer menu
 function hambMenu() {
     document.getElementById("menuHeader").style.display = "flex";
-}
+};
 
 function closeMenu() {
     document.getElementById("menuHeader").style.display = "none";
-}
+};
 
 function validaForm() {
-    let commo = document.getElementById("nameCommodity").value
-    let valueCommo = document.getElementById("valueCommodity").value
-    let typeShop = document.getElementById("type_deal").value
+    let commo = document.getElementById("nameCommodity").value;
+    let valueCommo = document.getElementById("valueCommodity").value;
+    let typeShop = document.getElementById("type_deal").value;
     // Checar se o form está preenchido
     if (commo == '') {
         document.getElementById("mercNameNotice").style.display = "block";
-        return false
+        return false;
     } else {
         document.getElementById("mercNameNotice").style.display = "none";
         if (valueCommo == '') {
             document.getElementById("mercValueNotice").style.display = "block";
-            return false
+            return false;
         } else {
             document.getElementById("mercValueNotice").style.display = "none";
             // Realizar a adição da tarefe no resultado de transação
             var sign = "+";
             if (typeShop == "1"){
                 sign = "-"
-            }
+            };
             if (divEmpty == 1){
                 document.getElementById("dealResul").innerHTML=""
                 divEmpty = 0
-            }
+            };
             document.getElementById("dealResul").innerHTML +=`
             <div class="merc">
                     <span>`+ sign +` `+ commo +`</span>
@@ -72,9 +72,10 @@ function validaForm() {
                 numSum += parseFloat(valueCommo.replace(".","").replace(",","."))*(-1.0);
             } else{
                 numSum += parseFloat(valueCommo.replace(".","").replace(",","."));
-            }
+            };
 
-            document.getElementById("totalValue").innerHTML = "R$ " + numSum.toString().replace("-","");
+            let numSuma = applyMask(numSum.toFixed(2).toString().replace("-","").replace(".",""));
+            document.getElementById("totalValue").innerHTML = "R$ " + numSuma;
 
             if(numSum < 0.0){
                 document.getElementById("statusValue").innerHTML = "[PREJUÍZO]";
@@ -82,17 +83,54 @@ function validaForm() {
                 document.getElementById("statusValue").innerHTML = "";
             } else{
                 document.getElementById("statusValue").innerHTML = "[LUCRO]";
-            }
+            };
 
-            document.querySelector("form").reset()
-            num = ""
-            return false
-        }
-    }
+            document.querySelector("form").reset();
+            num = "";
+            return false;
+        };
+    };
     
-}
+};
 
 // Mascará para número
+
+function applyMask(num){
+    let value = "";
+    if (num.length == 0) {
+        value = "";
+    } else if (num.length < 2) {
+        value = "0,0" + num;
+    } else if (num.length < 3) {
+        value = "0," + num;
+    } else {
+        let divi = (num.length - 2) / 3;
+        let pontoDivi = divi.toString().split('.');
+        if ( divi <= 1) {
+            value = num.slice(0, -2) + ',' + num.slice(-2);
+        } else {
+            let numPoint = ""
+            if(Number.isInteger(divi)){
+                for(let i=0;i<(parseInt(pontoDivi[0],10)-1);i++) {
+                    let numRest = ((num.length - 2) % 3) + (i*3);
+                    numPoint += "." + num.slice((numRest),(numRest+3));
+                };
+                value = num.slice(0, 3) + numPoint + ',' + num.slice(-2);
+            } else {
+                for(let i=0;i<parseInt(pontoDivi[0],10);i++) {
+                    let numRest = ((num.length - 2) % 3) + (i*3);
+                    numPoint += "." + num.slice((numRest),(numRest+3));
+                };
+                let restNum = (num.length - 2) % 3
+                value = num.slice(0, restNum) + numPoint  + ',' + num.slice(-2);
+            };
+            
+        };
+        
+    };
+    return value;
+};
+
 const input = document.getElementById("valueCommodity");
 let num = "";
 
@@ -103,40 +141,10 @@ function checkNum(e) {
     } else {
         num += e.key;
         if (e.key == "Backspace") {
-            num = num.slice(0, -10)
-        }
-        if (num.length == 0) {
-            e.target.value = ""
-        } else if (num.length < 2) {
-            e.target.value = "0,0" + num;
-        } else if (num.length < 3) {
-            e.target.value = "0," + num;
-        } else {
-            let divi = (num.length - 2) / 3
-            let pontoDivi = divi.toString().split('.');
-            if ( divi <= 1) {
-                e.target.value = num.slice(0, -2) + ',' + num.slice(-2);
-            } else {
-                let numPoint = ""
-                if(Number.isInteger(divi)){
-                    for(let i=0;i<(parseInt(pontoDivi[0],10)-1);i++) {
-                        let numRest = ((num.length - 2) % 3) + (i*3)
-                        numPoint += "." + num.slice((numRest),(numRest+3))
-                    }
-                    e.target.value = num.slice(0, 3) + numPoint + ',' + num.slice(-2);
-                } else {
-                    for(let i=0;i<parseInt(pontoDivi[0],10);i++) {
-                        let numRest = ((num.length - 2) % 3) + (i*3)
-                        numPoint += "." + num.slice((numRest),(numRest+3))
-                    }
-                    let restNum = (num.length - 2) % 3
-                    e.target.value = num.slice(0, restNum) + numPoint  + ',' + num.slice(-2);
-                }
-                
-            }
-            
-        }
-    }
-}
+            num = num.slice(0, -10);
+        };
+        e.target.value = applyMask(num)
+    };
+};
 
 input.addEventListener('keydown', checkNum);
